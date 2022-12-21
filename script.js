230c4c3ab20437136cc20f6ab3d6d93aff4ba628
richard <theirsaviorhd@gmail.com>
2022-12-21 05:59:07 -0500

    Animation Variation
    heading animations are the only elements that transform
    paragraph content has a fade in transition when observed
    javascript now has notes describing how it triggers the css or dom elements to change

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//when user observes a section, an animation triggers
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry => {
         if (entry.isIntersecting) {
@@ -7,13 +8,16 @@ const observer = new IntersectionObserver((entries) => {
         }
     }))
 })
-
+//change element of the hidden class to show
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
-
+//elements of the fade class don't trasnform. they only fade
+const hiddenFadeElements = document.querySelectorAll('.hidden-fade-only');
+hiddenFadeElements.forEach((el) => observer.observe(el));
+// trigger the number counter animation on page load
 const valueDisplays = document.querySelectorAll('.num');
 const interval = 5000;
-
+//change the display by increasing the number content from 0 to 1856
 valueDisplays.forEach((intDisplay) => {
     let startValue = 0;
     let endValue = parseInt(intDisplay.getAttribute('data-val'));
