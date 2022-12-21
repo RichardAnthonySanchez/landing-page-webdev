d81dc6ddbe1e4ea8bf6b6a6c71f82f1279f3f92d
richard <theirsaviorhd@gmail.com>
2022-12-21 04:27:45 -0500

    Counter Animation
    animation that counts the number above the fold displays
    it might look better if it took half the time to complete but it works

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const valueDisplays = document.querySelectorAll('.num');
+const interval = 5000;
+
+valueDisplays.forEach((intDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(intDisplay.getAttribute('data-val'));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        intDisplay.textContent = startValue + "+ satisfied customers";
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration
+    );
+});
